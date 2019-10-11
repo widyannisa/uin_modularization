764d8db976e0470c163e1601e76056527d727edf
widyannisa <56432495+widyannisa@users.noreply.github.com>
2019-10-11 16:53:59 +0700

    kecepatan kecepatan

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ program = 'Gerak Lurus'
 
 print(f'Program {program} oleh {nama}')
 
-jarak = 1000
-waktu = 5 * 60
-kecepatan = jarak / waktu
-print(f'Jarak = {jarak} ditempuh dalam waktu = {waktu/60}menit')
-print(f'Sehingga kecepatan = {kecepatan} m/s')
+def hitung_kecepatan(jarak, waktu):
+    kecepatan = jarak / waktu
+    print(f'Jarak = {jarak} ditempuh dalam waktu = {waktu / 60}menit')
+    print(f'Sehingga kecepatan = {kecepatan} m/s')
+    return kecepatan
+
+# jarak = 1000
+# waktu = 5 * 60
+kecepatan = hitung_kecepatan (1000, 5 * 60)
+kecepatan = hitung_kecepatan (3000, 70 * 60)
